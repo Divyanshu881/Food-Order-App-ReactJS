10a81daa8a2fb44e4ceb0182ce9e13ff4b184e09
Divyanshu <divyanshu.sharma881@gmail.com>
2022-06-14 21:26:58 +0530

    Adding a Modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Component/Layout/Header";
 import Meals from "./Component/Meals/Meals";
+import Cart from "./Component/Cart/Cart";
 function App() {
   return (
     <React.Fragment>
+      <Cart/>
       <Header/>
       <Meals/>
     </React.Fragment>
