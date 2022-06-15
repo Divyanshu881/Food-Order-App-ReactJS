2df1e2565cdf18ff56b6102660b03efb563df6bd
Divyanshu <divyanshu.sharma881@gmail.com>
2022-06-15 17:35:36 +0530

    btn bump using useEffects

--- src/Component/Layout/HeaderCartButton.js
+++ src/Component/Layout/HeaderCartButton.js
@@ -1,21 +1,31 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../Store/cart-context";
 
 const HeaderCartButton = (props) => {
   const ctx = useContext(CartContext);
-
+  const [btmBump, setBtnBump] = useState(false);
   const totalCartItem = ctx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const style = `${classes.button} ${btmBump ? classes.bump : ""}`;
+  useEffect(() => {
+    if (ctx.items.length === 0) {
+      return;
+    }
+    setBtnBump(true);
+    const timer = setTimeout(() => {
+      setBtnBump(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [ctx.items]);
   return (
-    <button
-      type={props.type}
-      className={classes.button}
-      onClick={props.onClick}
-    >
+    <button type={props.type} className={style} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
