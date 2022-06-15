ed4e73a4ea9019ebd3d1ab9c4ad5420b0b960401
Divyanshu <divyanshu.sharma881@gmail.com>
2022-06-15 14:44:13 +0530

    using reducer and ref state

--- src/Component/Meals/MealItem/MealItemForm.js
+++ src/Component/Meals/MealItem/MealItemForm.js
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
 
 const MealItemForm = (props) => {
+  const amountInputRef = useRef();
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const entredAmount = amountInputRef.current.value;
+    const numberEntredAmount = +entredAmount;
 
+    if (
+      entredAmount.trim().length === 0 ||
+      numberEntredAmount < 0 ||
+      numberEntredAmount > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    props.onAddToCart(numberEntredAmount);
+  };
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
-          id: 'amount_' + props.id,
+          id: "amount_" + props.id,
           type: "number",
           min: "0",
           max: "5",
-          step: '1',
+          step: "1",
           defaultValue: "0",
-          readOnly:true,
-  
+          // readOnly: true,
         }}
       />
-      
 
-      <button >+Add</button>
-      
+      <button>+Add</button>
+      {!amountIsValid && <p>Enter the valid amount between 0-5</p>}
     </form>
   );
 };
